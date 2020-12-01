27d165f8f7e587e59e4b43751086096f6ed667da
Kay Huber <kay.huber@unblu.com>
2020-12-01 22:09:16 +0100

    Finished puzzle01 for day01

--- day01/build/puzzle01.js
+++ day01/build/puzzle01.js
@@ -1,2 +1,30 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
 console.log("Puzzle 01!");
+var line_reader_1 = __importDefault(require("line-reader"));
+var bigNumberArray = [];
+var smallNumberArray = [];
+line_reader_1.default.eachLine("./input/input.txt", function (line, last) {
+    var current = parseInt(line);
+    if (current >= 1010) {
+        bigNumberArray.push(current);
+    }
+    else {
+        smallNumberArray.push(current);
+    }
+    if (last) {
+        var n = smallNumberArray.length;
+        for (var i = 0; i < n; i++) {
+            var m = bigNumberArray.length;
+            for (var j = 0; j < m; j++) {
+                if (smallNumberArray[i] + bigNumberArray[j] === 2020) {
+                    console.log("Riddle solution: " + (smallNumberArray[i] * bigNumberArray[j]));
+                }
+            }
+        }
+    }
+});
+//# sourceMappingURL=puzzle01.js.map
--- day01/src/puzzle01.ts
+++ day01/src/puzzle01.ts
@@ -1,1 +1,28 @@
 console.log("Puzzle 01!")
+
+import linereader from "line-reader";
+
+const bigNumberArray : number[] = [];
+const smallNumberArray : number[] = [];
+
+linereader.eachLine("./input/input.txt", (line, last) => {
+    const current = parseInt(line);
+    if (current >= 1010) {
+        bigNumberArray.push(current);
+    } else {
+        smallNumberArray.push(current);
+    }
+
+    if (last) {
+        const n = smallNumberArray.length;
+        for (let i=0; i < n; i++) {
+            const m = bigNumberArray.length;
+            for (let j=0; j < m; j++) {
+                if (smallNumberArray[i] + bigNumberArray[j] === 2020) {
+                    console.log("Riddle solution: " + (smallNumberArray[i] * bigNumberArray[j]));
+                }
+            }
+        }
+    }
+});
+
