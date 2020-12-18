a0f2ca8873178a984e5b8fe819ec3829040697c0
Kay Huber <kay.huber@unblu.com>
2020-12-18 23:43:35 +0100

    Implement day18 puzzle02

---
+++ day18/build/main/puzzle01.js
@@ -0,0 +1,125 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("Day 17, Puzzle 01!");
+const line_reader_1 = __importDefault(require("line-reader"));
+class NumberExpression {
+    constructor(number) {
+        this.number = number;
+    }
+    evaluate() {
+        return this.number;
+    }
+}
+class Operation {
+    evaluate() {
+        if (this.left === undefined) {
+            throw new Error("Left operand was undefined at time of evaluation");
+        }
+        if (this.right === undefined) {
+            throw new Error("Right operand was undefined at time of evaluation");
+        }
+        if (this.operator === "*") {
+            return this.left.evaluate() * this.right.evaluate();
+        }
+        else if (this.operator === "+") {
+            return this.left.evaluate() + this.right.evaluate();
+        }
+        else {
+            throw new Error("Invalid operator");
+        }
+    }
+}
+class ParenthesisExpression {
+    evaluate() {
+        if (this.containedExpression === undefined) {
+            throw new Error("Invalid parenthesis expression (no content)");
+        }
+        return this.containedExpression.evaluate();
+    }
+}
+var Token;
+(function (Token) {
+    Token["Whitespace"] = "^\\s+";
+    Token["Number"] = "^\\d+";
+    Token["ParenthesisOpen"] = "^\\(";
+    Token["ParenthesisClose"] = "^\\)";
+    Token["OperatorAdd"] = "^\\+";
+    Token["OperatorMultiply"] = "^\\*";
+})(Token || (Token = {}));
+function main() {
+    let sumOfAllLineResults = 0;
+    line_reader_1.default.eachLine("./input/input.txt", (line, last) => {
+        const clauses = [];
+        while (line.length > 0) {
+            if (line.match(Token.Whitespace)) {
+                // @ts-ignore
+                let match = line.match(Token.Whitespace)[0];
+                line = line.substring(match.length);
+            }
+            else if (line.match(Token.Number)) {
+                // @ts-ignore
+                let match = line.match(Token.Number)[0];
+                let number = new NumberExpression(parseInt(match));
+                let lastExpression = clauses[clauses.length - 1];
+                if (lastExpression !== undefined && lastExpression instanceof Operation) {
+                    lastExpression.right = number;
+                }
+                else {
+                    clauses.push(number);
+                }
+                line = line.substring(match.length);
+            }
+            else if (line.match(Token.OperatorAdd)) {
+                // @ts-ignore
+                let match = line.match(Token.OperatorAdd)[0];
+                let operation = new Operation();
+                operation.left = clauses.pop();
+                operation.operator = "+";
+                clauses.push(operation);
+                line = line.substring(match.length);
+            }
+            else if (line.match(Token.OperatorMultiply)) {
+                // @ts-ignore
+                let match = line.match(Token.OperatorMultiply)[0];
+                let operation = new Operation();
+                operation.left = clauses.pop();
+                operation.operator = "*";
+                clauses.push(operation);
+                line = line.substring(match.length);
+            }
+            else if (line.match(Token.ParenthesisOpen)) {
+                // @ts-ignore
+                let match = line.match(Token.ParenthesisOpen)[0];
+                clauses.push(new ParenthesisExpression());
+                line = line.substring(match.length);
+            }
+            else if (line.match(Token.ParenthesisClose)) {
+                // @ts-ignore
+                let match = line.match(Token.ParenthesisClose)[0];
+                let containedExpression = clauses.pop();
+                let parenthesisExpression = clauses.pop();
+                parenthesisExpression.containedExpression = containedExpression;
+                let lastExpression = clauses[clauses.length - 1];
+                if (lastExpression !== undefined && lastExpression instanceof Operation) {
+                    lastExpression.right = parenthesisExpression;
+                }
+                else {
+                    clauses.push(parenthesisExpression);
+                }
+                line = line.substring(match.length);
+            }
+        }
+        console.log("Line result: " + clauses[0].evaluate());
+        sumOfAllLineResults += clauses[0].evaluate();
+        if (last) {
+            console.log("Result: " + sumOfAllLineResults);
+        }
+    });
+}
+if (require.main === module) {
+    main();
+}
+//# sourceMappingURL=puzzle01.js.map
--- day18/build/main/puzzle02.js
+++ day18/build/main/puzzle02.js
@@ -1,166 +1,142 @@
-console.log("Day 17, Puzzle 02!")
-
-import linereader from "line-reader";
-
-interface Expression {
-    evaluate(): number;
-}
-
-class NumberExpression implements Expression {
-    number: number;
-
-    constructor(number: number) {
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("Day 17, Puzzle 02!");
+const line_reader_1 = __importDefault(require("line-reader"));
+class NumberExpression {
+    constructor(number) {
         this.number = number;
     }
-
-    evaluate(): number {
+    evaluate() {
         return this.number;
     }
 }
-
-class Operation implements Expression {
-    left: Expression | undefined;
-    right: Expression | undefined;
-    operator: "*" | "+" | undefined;
-
-    evaluate(): number {
+class Operation {
+    evaluate() {
         if (this.left === undefined) {
             throw new Error("Left operand was undefined at time of evaluation");
         }
-
         if (this.right === undefined) {
             throw new Error("Right operand was undefined at time of evaluation");
         }
-
         if (this.operator === "*") {
             return this.left.evaluate() * this.right.evaluate();
-        } else if (this.operator === "+") {
+        }
+        else if (this.operator === "+") {
             return this.left.evaluate() + this.right.evaluate();
-        } else {
+        }
+        else {
             throw new Error("Invalid operator");
         }
     }
 }
-
-class ParenthesisExpression implements Expression {
-    containedExpression: Expression | undefined;
-
-    evaluate(): number {
+class ParenthesisExpression {
+    evaluate() {
         if (this.containedExpression === undefined) {
-            throw new Error("Invalid parenthesis expression (no content)")
+            throw new Error("Invalid parenthesis expression (no content)");
         }
-
         return this.containedExpression.evaluate();
     }
 }
-
-enum Token {
-    Whitespace = "^\\s+",
-    Number = "^\\d+",
-    ParenthesisOpen = "^\\(",
-    ParenthesisClose = "^\\)",
-    OperatorAdd = "^\\+",
-    OperatorMultiply = "^\\*"
-}
-
+var Token;
+(function (Token) {
+    Token["Whitespace"] = "^\\s+";
+    Token["Number"] = "^\\d+";
+    Token["ParenthesisOpen"] = "^\\(";
+    Token["ParenthesisClose"] = "^\\)";
+    Token["OperatorAdd"] = "^\\+";
+    Token["OperatorMultiply"] = "^\\*";
+})(Token || (Token = {}));
 function main() {
-
     let sumOfAllLineResults = 0;
-    linereader.eachLine("./input/input.txt", (line, last) => {
-        let clauses: Expression[] = [];
+    line_reader_1.default.eachLine("./input/input.txt", (line, last) => {
+        let clauses = [];
         while (line.length > 0) {
             if (line.match(Token.Whitespace)) {
                 // @ts-ignore
                 let match = line.match(Token.Whitespace)[0];
                 line = line.substring(match.length);
-            } else if (line.match(Token.Number)) {
+            }
+            else if (line.match(Token.Number)) {
                 // @ts-ignore
                 let match = line.match(Token.Number)[0];
-
                 let number = new NumberExpression(parseInt(match));
-
                 if (clauses.length > 0 && (clauses[clauses.length - 1] instanceof Operation)) {
-                    let operation: Operation = clauses[clauses.length - 1] as Operation;
+                    let operation = clauses[clauses.length - 1];
                     while (operation.right !== undefined) {
-                        operation = operation.right as Operation;
+                        operation = operation.right;
                     }
                     operation.right = number;
-                } else {
+                }
+                else {
                     clauses.push(number);
                 }
-
                 line = line.substring(match.length);
-            } else if (line.match(Token.OperatorAdd)) {
+            }
+            else if (line.match(Token.OperatorAdd)) {
                 // @ts-ignore
                 let match = line.match(Token.OperatorAdd)[0];
-
-                let operation: Operation = new Operation();
+                let operation = new Operation();
                 operation.operator = "+";
-
                 let lastExpression = clauses.pop();
                 if (lastExpression instanceof Operation) {
-                    let lastOperation: Operation = lastExpression;
+                    let lastOperation = lastExpression;
                     operation.left = lastOperation.right;
                     lastOperation.right = operation;
                     clauses.push(lastOperation);
-                } else {
+                }
+                else {
                     operation.left = lastExpression;
                     clauses.push(operation);
                 }
-
                 line = line.substring(match.length);
-            } else if (line.match(Token.OperatorMultiply)) {
+            }
+            else if (line.match(Token.OperatorMultiply)) {
                 // @ts-ignore
                 let match = line.match(Token.OperatorMultiply)[0];
-
-                let operation: Operation = new Operation();
+                let operation = new Operation();
                 operation.operator = "*";
-
                 let lastExpression = clauses.pop();
                 operation.left = lastExpression;
                 clauses.push(operation);
-
                 line = line.substring(match.length);
-            } else if (line.match(Token.ParenthesisOpen)) {
+            }
+            else if (line.match(Token.ParenthesisOpen)) {
                 // @ts-ignore
                 let match = line.match(Token.ParenthesisOpen)[0];
-
                 clauses.push(new ParenthesisExpression());
-
                 line = line.substring(match.length);
-            } else if (line.match(Token.ParenthesisClose)) {
+            }
+            else if (line.match(Token.ParenthesisClose)) {
                 // @ts-ignore
                 let match = line.match(Token.ParenthesisClose)[0];
-
                 let containedExpression = clauses.pop();
-                let parenthesisExpression: ParenthesisExpression = clauses.pop() as ParenthesisExpression;
+                let parenthesisExpression = clauses.pop();
                 parenthesisExpression.containedExpression = containedExpression;
-
                 let lastExpression = clauses[clauses.length - 1];
                 if (lastExpression !== undefined && lastExpression instanceof Operation) {
-                    let operation: Operation = clauses[clauses.length - 1] as Operation;
+                    let operation = clauses[clauses.length - 1];
                     while (operation.right !== undefined) {
-                        operation = operation.right as Operation;
+                        operation = operation.right;
                     }
                     operation.right = parenthesisExpression;
-                } else {
+                }
+                else {
                     clauses.push(parenthesisExpression);
                 }
-
                 line = line.substring(match.length);
             }
         }
-
         console.log("Line result: " + clauses[0].evaluate());
         sumOfAllLineResults += clauses[0].evaluate();
-
         if (last) {
             console.log("Result: " + sumOfAllLineResults);
         }
     });
 }
-
 if (require.main === module) {
     main();
 }
-
+//# sourceMappingURL=puzzle02.js.map
